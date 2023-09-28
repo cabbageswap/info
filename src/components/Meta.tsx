e8cb348d4a61912d828e60773a76d6439c2d5856
cabbagemichael <michael@cabbageswap.com>
2023-09-28 10:21:59 +0700

    fix: updated trade link

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
 import React from 'react'
 
-const WEBSITE_HOST_URL = 'https://info.uniswap.org'
+const WEBSITE_HOST_URL = 'https://info.cababgeswap.com'
 
 const Meta = ({ location }: RouteComponentProps): JSX.Element => {
   return (
