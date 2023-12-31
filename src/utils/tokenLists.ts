ff1dded133bafbeb22ad0d2dfb1d70777c44cbff
cabbagemichael <michael@cabbageswap.com>
2023-12-31 23:58:47 +0700

    chore: updated node version

--- src/utils/tokenLists.ts
+++ src/utils/tokenLists.ts
@@ -1,7 +1,6 @@
 import { TokenList } from '@uniswap/token-lists'
-import schema from '@uniswap/token-lists/src/tokenlist.schema.json'
 import Ajv from 'ajv'
-
+const schema = require('@uniswap/token-lists/src/tokenlist.schema.json');
 /**
  * Given a URI that may be ipfs, ipns, http, or https protocol, return the fetch-able http(s) URLs for the same content
  * @param uri to convert to fetch-able http url
